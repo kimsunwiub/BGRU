ddd5f82ce020e6ba83acf5b28441bb82618e9a00
Sun Woo Kim <kimsunw@indiana.edu>
2018-05-24 17:45:50 -0400

    Deleting saved models and files

--- run_multi_gpu.sh
+++ run_multi_gpu.sh
@@ -1,10 +1,10 @@
 # python main.py 100 7e-5 1 0 &
 # python main.py 100 7e-5 1 1 &
 # python main.py 100 3e-5 1 2 &
-python main.py 5e-5 5 -1 200 3 &
-python main.py 4e-5 5 -1 200 4 &
-python main.py 3e-5 5 -1 200 5 &
-python main.py 2e-5 5 30 200 6 &
-python main.py 5e-5 5 30 200 7 &
+python main.py 2e-5 5 -1 2000 3 &
+python main.py 2e-5 5 -1 2000 4 &
+python main.py 2e-5 5 30 2000 5 &
+python main.py 2e-5 5 30 2000 6 &
+python main.py 2e-5 5 60 2000 7 &
 # Next b=1
 # And make more description in imgs or filenames
